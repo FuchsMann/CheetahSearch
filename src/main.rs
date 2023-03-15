06537b89496592a51478bf784a7b4e8be399b795
FuchsMann <kommandantfuchs@protonmail.com>
2023-03-15 21:07:47 +0100

    n

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,3 @@ use chee_ui::app_init;
 fn main() {
     app_init().unwrap();
 }
-
-/* 
-fn main() {
-    println!("S INIT");
-    let mut search = Search::new(
-        String::from("/Users/timstammwitz/Documents"),
-        vec![String::from("search")],
-        vec![String::from(".rs"), String::from("cheese")],
-    );
-
-    println!("Starting Index.");
-    search.index_filepaths();
-    println!("Starting search.");
-    search.parallel_search();
-    println!("Hits: {}", search.search_hits.len());
-    for search_hit in &search.search_hits {
-        println!("{}", search_hit.to_string());
-    }
-}
-*/
